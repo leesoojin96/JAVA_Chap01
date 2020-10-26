cf4a18b69251e992b856bb6e71c553fff2d627fb
leesoojin96 <69374300+leesoojin96@users.noreply.github.com>
2020-10-26 16:26:43 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+package sec01_03.exam01;
+
+/* 
+ 여러 줄을 주석으로 해줍니다.
+ 자바 공부시작 2020.06.20
+ */
+public class Hello {
+
+	// 프로그램실행 진입시작점  
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello,JAVA");
+
+	}//메소드 선언부 - 현재 메소드 이름 : main => and of method
+
+} //클래스 선언부 and of class
---
+++ RunStatementExample.java
@@ -0,0 +1,15 @@
+package sec01_03.exam02;
+
+public class RunStatementExample {
+
+	public static void main(String[] args) {
+		
+		int x = 1; // 초기값 설정  -> 초기값이 없으면 컴파일 에러 발생
+		int y = 2;
+		int result = x + y;
+		
+		System.out.println(result);
+
+	}
+
+}
